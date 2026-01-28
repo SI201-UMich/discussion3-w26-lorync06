570be78a9abb911d0872f087717fcd91b80f413c
Loryn Canty <loryncanty@Loryns-MacBook-Air-4.local>
2026-01-28 17:42:34 -0500

    code

--- Discussion3.py
+++ Discussion3.py
@@ -21,34 +21,34 @@ class Rectangle():
         return self.width * self.height
 
     # Create the "__eq__" method
-    # 
-    # Returns a boolean value
 
-    # YOUR CODE HERE
+
     def __eq__(self, other):
+        if self.width == other.width and self.height == other.height:
+            return True
+        else:
+            return False
       
 
     
-
-
 def main():
-    r1 = Rectangle(10, 10)
-    # call the __str__ method
-    print(r1)
-    # call the area_calculator method
-    print("Area:", r1.area_calculator())
+        r1 = Rectangle(10, 10)
+        # call the __str__ method
+        print(r1)
+        # call the area_calculator method
+        print("Area:", r1.area_calculator())
 
 
-    r2 = Rectangle(10, 15)
-    print(r2)
-    print("Area:", r2.area_calculator())
-    # call the __eq__ method
-    print(r1 == r2)
-    print()
+        r2 = Rectangle(10, 15)
+        print(r2)
+        print("Area:", r2.area_calculator())
+        # call the __eq__ method
+        print(r1 == r2)
+        print()
 
-    # you can create additional rectangle objects to 
-    # test your code or learn more about how the class behaves
-    pass
+        # you can create additional rectangle objects to 
+        # test your code or learn more about how the class behaves
+        pass
 
 if __name__ == "__main__":
     main()
